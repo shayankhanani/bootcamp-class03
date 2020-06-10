3637d59d9901935da8cdb413a8de5ea9668edcad
Shayan Khanani <45587698+shayankhanani@users.noreply.github.com>
2020-06-10 23:20:58 +0500

    Assignment # 3 From Bootcamp 2020 Class 03 Using https://daveceddia.com/react-tutorial/ React Tutorial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './card.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <h3>Hello from Shayan</h3>
+      <p>This is Bootcamp 2020 class 03, and we are discussing about React Basics</p>
+      <h5>Mentors</h5>
+      <ul>
+        <li>Sir Zia Khan</li>
+        <li>Sir Zeeshan Hanif</li>
+      </ul>
+      <p>We discussed that if there are no <strong>brackets</strong> 2 + 2 = 22 but if we used <strong>brackets</strong> 2 + 2 = {2 + 2}</p>
+      <div className="card">
+        This is the <strong>CSS Magic</strong>
+      </div>
     </div>
   );
 }
